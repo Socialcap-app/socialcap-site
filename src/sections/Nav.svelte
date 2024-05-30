dfc48954ca8a345901aff9e9c962f114748bb6fd
fabri2605 <fabridp77@gmail.com>
2024-05-30 14:08:28 -0300

    logo-added

--- src/sections/Nav.svelte
+++ src/sections/Nav.svelte
@@ -8,9 +8,11 @@
         NavUl,
         Navbar,
     } from 'flowbite-svelte';
+
+    import Logo from '../components/Logo.svg';
 </script>
 
 <Navbar class={'bg-transparent p-2 '}>
-    <h2>socialcap</h2>
+    <img alt="logo" src={Logo} />
     <Button class={'px-5 text-sm font-normal'} color="blue">Sign in</Button>
 </Navbar>
